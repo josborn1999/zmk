48e4459ee8560e4b6faef01f4991a3e347a3cdbd
josborn1999 <90360085+josborn1999@users.noreply.github.com>
2024-08-17 13:11:16 -0500

    Update peripheral_status.c

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -26,6 +26,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 LV_IMG_DECLARE(balloon);
 LV_IMG_DECLARE(mountain);
 LV_IMG_DECLARE(arasaka);
+LV_IMG_DECLARE(arasaka_master);
 LV_IMG_DECLARE(gigi);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
@@ -118,7 +119,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t *art = lv_img_create(widget->obj);
     bool random = sys_rand32_get() & 1;
-    lv_img_set_src(art, &gigi);
+    lv_img_set_src(art, &arasaka_master);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
